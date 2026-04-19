79e77e9df57c058589d9a8ff9259da3eb98b991c
Jiadezhende <3139966468@qq.com>
2026-04-20 00:42:24 +0800

    feat: 添加run.sh用于在线评测

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+pip3 install python-dotenv jsonschema \
+  -i https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple \
+  --default-timeout 30
+
+python3 /workspace/main.py \
+  --spec /target/target_spec.json \
+  --output /workspace/output.json \
+  --verbose
